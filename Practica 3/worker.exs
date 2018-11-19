274f655d71d1f5fa3d75a8cdac443f25a5b18abd
aznarjorge <jorgeaznar_97@hotmail.com>
2018-11-19 19:13:00 +0100

    ejercicio3

--- Practica 3/worker.exs
+++ Practica 3/worker.exs
@@ -3,35 +3,48 @@
 # FICHERO: chat.exs
 # FECHA: 7 de noviembre de 2018
 # TIEMPO: 9 horas 
-# DESCRIPCI'ON: codigo correspondiente al chat a desarrollar en la practica 2
+# DESCRIPCION: codigo correspondiente al chat a desarrollar en la practica 2
 
 defmodule Worker do
-	def listadivisores(1,n,lista) do
-		lista= [1|lista]
+
+	def listaDivisores(1,n,lista) do
+		lista = [1 | lista]
 	end
-	def listadivisores(m,n,lista) do
+	
+	def listaDivisores(m,n,lista) do
 		cond do
-		rem(n,m)==0  -> lista= [m|lista]
-						listadivisores(m-1,n,lista)
-		true     ->		listadivisores(m-1,n,lista)
+		  rem(n,m) == 0  -> lista = [m | lista]
+							listaDivisores(m - 1,n,lista)
+		  true     		 ->	listaDivisores(m - 1,n,lista)
 		end
 	end
+	
 	def divisores(n) do
-		listadivisores(div(n,2),n,[n])
+	  listaDivisores(:math.sqrt(n) + 1,n,[])
 	end
-	def sumaLista([],total) do
-		cuentatotal=total
+	
+	def sumaLista([],total,pid) do
+	  cuentaTotal = total
+	  send(pid,{self,cuentaTotal})
 	end
-	def sumaLista(lista,total) do
-		total= total+ hd(lista)
-		sumaLista(tl(lista),total)
+	
+	def sumaLista(lista,total,pid) do
+	  total= total + hd(lista)
+	  sumaLista(tl(lista),total,pid)
 	end
-	def sumaListaDivisores(n) do
-		lista= listadivisores(div(n,2),n,[n])
-		sumaLista(lista,0)
+	
+	def sumaListaDivisores(n,pid) do
+	  lista = listaDivisores(:math.sqrt(n) + 1,n,[])
+	  sumaLista(lista,0,pid)
 	end
 	
 	def listaSumada(lista) do
 		sumaLista(lista,0)
 	end
+	
+	def worker() do
+		receive do
+			{pid,i,:sumaListaDivisores} -> sumaListaDivisores(i,pid)
+		end
+		worker()
 end
