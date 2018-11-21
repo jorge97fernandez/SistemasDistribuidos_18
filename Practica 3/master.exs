822c1bb21e25117413170f57a4c01185a3d9dffe
JORGE FM <jorge97fernandez@gmail.com>
2018-11-21 23:16:22 +0100

    VDef?

--- Practica 3/master.exs
+++ Practica 3/master.exs
@@ -6,135 +6,180 @@
 # DESCRIPCION: codigo correspondiente al chat a desarrollar en la practica 2
 
 defmodule Master do
-	def encontrar_amigos_rec_2_1(n, amigos, [],worker13, i, y) do
-		encontrar_amigos_rec_13_2(n, amigos, worker, i,[],y)
+	def encontrar_amigos_rec_2_1(n, amigos, [],worker1,worker3, i, y) do
+		encontrar_amigos_rec_13_2(n, amigos, worker1,worker3, i,[],y)
 	end
-	def encontrar_amigos_rec_2_1(n, amigos, worker2,worker13, i,y) do
+	def encontrar_amigos_rec_2_1(n, amigos, worker2,worker1,worker3, i,y) do
 	send(hd(worker2), {self(), y, :sumaListaDivisores})
 							receive do
-								{pid, x} -> cond do
+								{pid, x,n} -> if (n== y) do
+											 cond do
 											 x == i && y != i && y > i ->
-																  encontrar_amigos_rec_2(n, [{i, y}| amigos], worker, i + 1)
+																  encontrar_amigos_rec_2(n, [{i, y}| amigos], worker2,worker1,worker3, i + 1)
 										     true			  -> 			
-																  encontrar_amigos_rec_2(n, amigos, worker, i + 1)
-										   end
-								after 1000 -> encontrar_amigos_rec_2_1(n,amigos,tl(worker2),worker13,i,y)
+																  encontrar_amigos_rec_2(n, amigos, worker2,worker1,worker3, i + 1)
+											end
+											else
+											encontrar_amigos_rec_2_1(n,amigos,worker2,worker1,worker3,i,y)
+											end
+								after 1000 -> encontrar_amigos_rec_2_1(n,amigos,tl(worker2),worker1,worker3,i,y)
 							end
 	end
-	def encontrar_amigos_rec_2(n, amigos, worker2,worker13, 1000001) do
+	def encontrar_amigos_rec_2(n, amigos, worker2,worker1,worker3, 1000001) do
 	  lista = amigos
 	end
-	def encontrar_amigos_rec_2(n, amigos, [],worker13, i) do
-		encontrar_amigos_rec_13(n, amigos, worker13, i) do
+	def encontrar_amigos_rec_2(n, amigos, [],worker1,worker3, i) do
+		encontrar_amigos_rec_13(n, amigos, worker1,worker3, i)
 	end
-	def encontrar_amigos_rec_2(n, amigos, worker2,worker13, i) do
+	def encontrar_amigos_rec_2(n, amigos, worker2,worker1,worker3, i) do
 		send(hd(worker2), {self(), i, :sumaListaDivisores})
 		receive do
-			{pid, y}   ->	IO.puts(i)
+			{pid, y,n }   ->cond do
+							n== i ->
+							IO.puts("Ey")
+							IO.puts(i)
 							send(hd(worker2), {self(), y, :sumaListaDivisores})
 							receive do
-								{pid, x} -> cond do
+								{pid, x,n} -> cond do
 											 x == i && y != i && y > i ->
-																  encontrar_amigos_rec_2(n, [{i, y}| amigos], worker, i + 1)
+																  encontrar_amigos_rec_2(n, [{i, y}| amigos], worker2,worker1,worker3, i + 1)
 										     true			  -> 			
-																  encontrar_amigos_rec_2(n, amigos, worker, i + 1)
-										   end
-								after 1000 -> encontrar_amigos_rec_2_1(n,amigos,tl(worker2),worker13,i,y)
+																  encontrar_amigos_rec_2(n, amigos, worker2,worker1,worker3, i + 1)
+											end
+								after 1000 -> encontrar_amigos_rec_2_1(n,amigos,tl(worker2),worker1,worker3,i,y)
 							end
-			after 1000 -> encontrar_amigos_rec_2(n,amigos,tl(worker2),worker13,i)
+						true -> encontrar_amigos_rec_2(n,amigos,tl(worker2),worker1,worker3,i)
+						end
+			after 1000 -> encontrar_amigos_rec_2(n,amigos,tl(worker2),worker1,worker3,i)
 		end
 	end
 
-	def encontrar_amigos(n, amigos, worker2,worker13,tipo) do
-		encontrar_amigos_rec_2(n, amigos, worker2,worker13, 1)
+	def encontrar_amigos(n, amigos, worker2,worker1,worker3,tipo) do
+		encontrar_amigos_rec_2(n, amigos, worker2,worker1,worker3, 1)
 	end
-	end
-	def encontrar_amigos_rec_13_3(n, amigos, [], i,y,x,y2) do
+	def encontrar_amigos_rec_13_3(n, amigos, worker1,[], i,y,x,y2) do
 		IO.puts("No quedan workers, me voy")
 	end
-	def encontrar_amigos_rec_13_3(n, amigos, worker, i,y,x,y2) do
-		send(hd(worker), {self(), y2, :sumaLista})
+	def encontrar_amigos_rec_13_3(n, amigos, worker1,worker3, i,y,x,y2) do
+		send(hd(worker3), {self(), y2, :sumaLista})
 																receive do
-																	{pid,x2}      ->cond do
+																	{pid,x2}      ->if (pid==hd(worker3)) do
+																					cond do
 																					x2 == i && x != i && x > i ->
-																									encontrar_amigos_rec_13(n, [{i, x}| amigos], worker, i + 1)
+																									encontrar_amigos_rec_13(n, [{i, x}| amigos], worker1,worker3, i + 1)
 																					true			  		-> 			
-																					encontrar_amigos_rec_13(n, amigos, worker, i + 1)
+																					encontrar_amigos_rec_13(n, amigos, worker1,worker3, i + 1)
 																					end
-																	after 1000    ->encontrar_amigos_rec_13_3(n, amigos, tl(worker), i,y,x,y2)
+																					end
+																	after 1000    ->encontrar_amigos_rec_13_3(n, amigos, worker1,tl(worker3), i,y,x,y2)
 																end
 	end
-	def encontrar_amigos_rec_13_2(n, amigos, worker, i,y,x) do
+	def encontrar_amigos_rec_13_2(n, amigos, [],worker3, i,y,x) do
 		IO.puts("No quedan workers, me voy")
 	end
-	def encontrar_amigos_rec_13_2(n, amigos, worker, i,y,x) do
-		send(hd(worker), {self(), x, :listaDivisores})
+	def encontrar_amigos_rec_13_2(n, amigos, worker1,[], i,y,x) do
+		IO.puts("No quedan workers, me voy")
+	end
+	def encontrar_amigos_rec_13_2(n, amigos, worker1,worker3, i,y,x) do
+		send(hd(worker1), {self(), x, :listaDivisores})
 											receive do
-												{pid, y2}   ->	send(hd(worker), {self(), y2, :sumaLista})
+												{pid, y2}   ->	if (pid== hd(worker1)) do
+																send(hd(worker3), {self(), y2, :sumaLista})
 																receive do
-																	{pid,x2}      ->cond do
+																	{pid,x2}      ->if (pid == hd(worker3)) do
+																					cond do
 																					x2 == i && x != i && x > i ->
-																									encontrar_amigos_rec_13(n, [{i, x}| amigos], worker, i + 1)
+																									encontrar_amigos_rec_13(n, [{i, x}| amigos], worker1,worker3, i + 1)
 																					true			  		-> 			
-																					encontrar_amigos_rec_13(n, amigos, worker, i + 1)
+																					encontrar_amigos_rec_13(n, amigos, worker1,worker3, i + 1)
 																					end
-																	after 1000    ->encontrar_amigos_rec_13_3(n, amigos, tl(worker), i,y,x,y2)
+																					end
+																	after 1000    ->encontrar_amigos_rec_13_3(n, amigos, worker1,tl(worker3), i,y,x,y2)
 																end
-												after 1000 ->encontrar_amigos_rec_13_2(n, amigos, tl(worker), i,y,x)
+																end
+												after 1000 ->encontrar_amigos_rec_13_2(n, amigos, tl(worker1),worker3, i,y,x)
 											end
 	end
-	def encontrar_amigos_rec_13_1(n, amigos, [], i,y) do
+	def encontrar_amigos_rec_13_1(n, amigos, [],worker3, i,y) do
 		IO.puts("No quedan workers, me voy")
 	end
-	def encontrar_amigos_rec_13_1(n, amigos, worker, i,y) do
-		send(hd(worker), {self(), y, :sumaLista})
+	def encontrar_amigos_rec_13_1(n, amigos, worker1,[], i,y) do
+		IO.puts("No quedan workers, me voy")
+	end
+	def encontrar_amigos_rec_13_1(n, amigos, worker1,worker3, i,y) do
+		send(hd(worker3), {self(), y, :sumaLista})
 							receive do
-								{pid, x} -> send(hd(worker), {self(), x, :listaDivisores})
+								{pid, x} -> if (pid== hd(worker3)) do
+											send(hd(worker1), {self(), x, :listaDivisores})
 											receive do
-												{pid, y2}   ->	send(hd(worker), {self(), y2, :sumaLista})
+												{pid, y2}   ->	if (pid == hd(worker1)) do
+																send(hd(worker3), {self(), y2, :sumaLista})
 																receive do
-																	{pid,x2}      ->cond do
+																	{pid,x2}      ->if (pid == hd(worker3)) do
+																					cond do
 																					x2 == i && x != i && x > i ->
-																									encontrar_amigos_rec_13(n, [{i, x}| amigos], worker, i + 1)
+																									encontrar_amigos_rec_13(n, [{i, x}| amigos], worker1,worker3, i + 1)
 																					true			  		-> 			
-																					encontrar_amigos_rec_13(n, amigos, worker, i + 1)
+																					encontrar_amigos_rec_13(n, amigos, worker1,worker3, i + 1)
 																					end
-																	after 1000    ->encontrar_amigos_rec_13_3(n, amigos, tl(worker), i,y,x,y2)
+																					end
+																	after 1000    ->encontrar_amigos_rec_13_3(n, amigos, worker1,tl(worker3), i,y,x,y2)
 																end
-												after 1000 ->encontrar_amigos_rec_13_2(n, amigos, tl(worker), i,y,x)
+																end
+												after 1000 ->encontrar_amigos_rec_13_2(n, amigos, tl(worker1),worker3, i,y,x)
 											end
-								after 1000 ->encontrar_amigos_rec_13_1(n, amigos, tl(worker), i,y)
+											end
+								after 1000 ->encontrar_amigos_rec_13_1(n, amigos, worker1,tl(worker3), i,y)
 							end
 	end
-	def encontrar_amigos_rec_13(n, amigos, [], i) do
+	def encontrar_amigos_rec_13(n, amigos,[],worker3,i) do
 		IO.puts("No quedan workers, me voy")
 	end
-	def encontrar_amigos_rec_13(n, amigos, worker, 1000001) do
+	def encontrar_amigos_rec_13(n, amigos, worker1,[],i) do
+		IO.puts("No quedan workers, me voy")
+	end
+	def encontrar_amigos_rec_13(n, amigos, worker1,worker3, 1000001) do
 	  lista = amigos
 	end
-	def encontrar_amigos_rec_13(n, amigos, worker, i) do
-		send(hd(worker), {self(), i, :listaDivisores})
+	def encontrar_amigos_rec_13(n, amigos, worker1,worker3, i) do
+		send(hd(worker1), {self(), i, :listaDivisores})
 		receive do
-			{pid, y}   ->	IO.puts(i)
-							send(hd(worker), {self(), y, :sumaLista})
+			{pid, y,n}   ->	if(n == i) do
+							IO.puts(i)
+							send(hd(worker3), {self(), y, :sumaLista})
 							receive do
-								{pid, x} -> send(hd(worker), {self(), x, :listaDivisores})
+								{pid, x,n} -> if ( n == y) do
+											send(hd(worker1), {self(), x, :listaDivisores})
 											receive do
-												{pid, y2}   ->	send(hd(worker), {self(), y2, :sumaLista})
+												{pid, y2,n}   ->	if (n == x) do
+																send(hd(worker3), {self(), y2, :sumaLista})
 																receive do
-																	{pid,x2}      ->cond do
+																	{pid,x2,n}      ->if (n == y2) do
+																					cond do
 																					x2 == i && x != i && x > i ->
-																									encontrar_amigos_rec_13(n, [{i, x}| amigos], worker, i + 1)
+																									encontrar_amigos_rec_13(n, [{i, x}| amigos], worker1,worker3, i + 1)
 																					true			  		-> 			
-																					encontrar_amigos_rec_13(n, amigos, worker, i + 1)
+																					encontrar_amigos_rec_13(n, amigos, worker1,worker3, i + 1)
 																					end
-																	after 1000    ->encontrar_amigos_rec_13_3(n, amigos, tl(worker), i,y,x,y2)
+																					else
+																					encontrar_amigos_rec_13_3(n, amigos, worker1,worker3, i,y,x,y2)
+																					end
+																	after 1000    ->encontrar_amigos_rec_13_3(n, amigos, worker1,tl(worker3), i,y,x,y2)
 																end
-												after 1000 ->encontrar_amigos_rec_13_2(n, amigos, tl(worker), i,y,x)
+																else
+																encontrar_amigos_rec_13_2(n, amigos, worker1,worker3, i,y,x)
+																end
+												after 1000 ->encontrar_amigos_rec_13_2(n, amigos, tl(worker1),worker3, i,y,x)
 											end
-								after 1000 ->encontrar_amigos_rec_13_1(n, amigos, tl(worker), i,y)
+											else
+											encontrar_amigos_rec_13_1(n, amigos, worker1,worker3, i,y)
+											end
+								after 1000 ->encontrar_amigos_rec_13_1(n, amigos, worker1,tl(worker3), i,y)
 							end
-			after 1000 -> encontrar_amigos_rec_13(n, amigos, tl(worker), i)
+							else
+							encontrar_amigos_rec_13(n, amigos, worker1,worker3, i)
+							end
+			after 1000 -> encontrar_amigos_rec_13(n, amigos, tl(worker1),worker3, i)
 		end
 	end
 end
